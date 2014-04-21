b990b03f14f4a18986932c4966ad69e45264fe8e
Shawn Crigger <support@s-vizion.com>
2014-04-20 23:12:54 -0400

    tidying up html format

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
   include './inc/head.php';
   include './inc/topbar.php';
-
-?>
-
+/*
 <!--
     <div class="row">
       <div class="large-12 columns">
-      	<div class="panel">
-	        <h3>I am stoked you have a few moments to look over my resume! </h3>
+        <div class="panel">
+          <h3>I am stoked you have a few moments to look over my resume! </h3>
 
           <div class="row clearfix">
             <div class="large-4 medium-4 columns">
@@ -25,93 +23,96 @@
               </p>
             </div>
 
-					</div>
-      	</div>
+          </div>
+        </div>
       </div>
     </div>
 -->
-
-
-    <div class="row clearfix">
-      <div class="large-4 medium-4 columns clearfix">
-        <p>
-          <img src="/img/heroshot-3.jpg" class="hero-img" alt="Picture of Shawn Crigger" />
-        </p>
-      </div>
-
-
-      <div class="large-6 medium-6 columns">
-
-        <h3>Career Objective</h3>
-          <p class="ptext">
-          I am a normal geek who has a deep interest in energy and electricity.  I am a natural at the art of diagnosing anything in my world, which has made me a great service tech.  I am a completely self taught computer programmer, creator, coder and craftsmen. Other than computers, I like music, nature, oceans and life!<br>
-          My passion for developing websites, building servers, and hopefully promoting music in some way makes me a very positive happy person which brings great energy into your company.  Add to that the years of experience I have under my belt, my willingness to learn or to teach depending on what you need.  <br>
-          My goal is to work for a quality web development company, preferably in the local music industry, or any web development company as a senior level PHP programmer.
-          </p>
-      </div>
-
-      <div class="large-2 medium-2 columns">
-      <h3>Contact Information</h3>
-        <ul class="vcard">
-          <li class="fn">Shawn Crigger</li>
-          <li class="street-address">2400 Douglas St.</li>
-          <li class="locality">North Myrtle Beach</li>
-          <li><span class="state">South Carolina</span>, <span class="zip">29582</span></li>
-          <li class="email"><a href="#">ithippyshawn@</a></li>
-        </ul>
-      </div>
-
-<!--
+*/
+?>
+        <div class="row clearfix">
+            <div class="large-4 medium-4 columns clearfix">
+                <p>
+                    <img src="/img/heroshot-3.jpg" class="hero-img" alt="Picture of Shawn Crigger">
+                </p>
+            </div>
+            <div class="large-6 medium-6 columns">
+                <h3>
+                    Career Objective
+                </h3>
+                <p class="ptext">
+                    I am a normal geek who has a deep interest in energy and electricity. I am a natural at the art of diagnosing anything in my world, which has made me a great service tech. I am a completely self taught computer programmer, creator, coder and craftsmen. Other than computers, I like music, nature, oceans and life!<br>
+                    My passion for developing websites, building servers, and hopefully promoting music in some way makes me a very positive happy person which brings great energy into your company. Add to that the years of experience I have under my belt, my willingness to learn or to teach depending on what you need.<br>
+                    My goal is to work for a quality web development company, preferably in the local music industry, or any web development company as a senior level PHP programmer.
+                </p>
+            </div>
+            <div class="large-2 medium-2 columns">
+                <h3>
+                    Contact Information
+                </h3>
+                <ul class="vcard">
+                    <li class="fn">Shawn Crigger
+                    </li>
+                    <li class="street-address">2400 Douglas St.
+                    </li>
+                    <li class="locality">North Myrtle Beach
+                    </li>
+                    <li>
+                        <span class="state">South Carolina</span>, <span class="zip">29582</span>
+                    </li>
+                    <li class="email">
+                        <a href="#">ithippyshawn@</a>
+                    </li>
+                </ul>
+            </div><!--
       <div class="large-4 medium-4 columns">
-			  <h5>Try one of these buttons:</h5>
-			  <p><a href="#" class="small button">Simple Button</a><br/>
+        <h5>Try one of these buttons:</h5>
+        <p><a href="#" class="small button">Simple Button</a><br/>
         <a href="#" class="small radius button">Radius Button</a><br/>
         <a href="#" class="small round button">Round Button</a><br/>
         <a href="#" class="medium success button">Success Btn</a><br/>
         <a href="#" class="medium alert button">Alert Btn</a><br/>
         <a href="#" class="medium secondary button">Secondary Btn</a></p>
-				<div class="panel">
-        	<h5>So many components, girl!</h5>
-        	<p>A whole kitchen sink of goodies comes with Foundation. Checkout the docs to see them all, along with details on making them your own.</p>
-        	<a href="http://foundation.zurb.com/docs/" class="small button">Go to Foundation Docs</a>
+        <div class="panel">
+          <h5>So many components, girl!</h5>
+          <p>A whole kitchen sink of goodies comes with Foundation. Checkout the docs to see them all, along with details on making them your own.</p>
+          <a href="http://foundation.zurb.com/docs/" class="small button">Go to Foundation Docs</a>
         </div>
       </div>
 
   -->
-
-    </div> <!-- end row -->
-<hr/>
-    <div class="row">
-      <div class="large-4 medium-4 columns">
-        <h3 style="padding-left: 30%;">Skill Levels</h3>
-        <ul class="ranking">
-          <li>PHP<span class="star-5">* * * * *</span></li>
-          <li>JavaScript<span class="star-5">* * * * *</span></li>
-          <li>CSS3<span class="star-4">* * * *</span></li>
-          <li>HTML5<span class="star-4">* * * *</span></li>
-          <li>XHTML<span class="star-3">* * *</span></li>
-          <li>Servers<span class="star-4">* * * *</span></li>
-        </ul>
-      </div>
-
-      <div class="large-8 medium-6 columns">
-        <h3>Programming Languages</h3>
-        <p class="ptext">
-          With PHP, I prefer to write class based module code, as I find it more reusable and cleaner.
-
-          JavaScript I can write without a framework if it's something that a framework would be overkill on size, I also am very familiar with jQuery and MooTools, also Ender and some of the newer micro libraries.  I have released many jQuery and MooTools snippets and plugins that were released open source.
-
-          Linux servers, I love working on servers and have built and maintained many of them for my personal use or business use.  I also love building computers and hacking hardware, I currently have 2 servers in my house just for home media centers.
-
-          General IT support, I can install software, remove software, viruses, malware, reinstall OSes, hook up your wifi printer, build a NAS device for your home network, whatever the job requires.  I do quite a bit of free lance general IT support currently.
-        </p>
-      </div>
-    </div> <!-- end row -->
-
-<hr/>
-    <div class="row">
-
-
-    </div> <!-- end row -->
+        </div><!-- end row -->
+        <hr>
+        <div class="row">
+            <div class="large-4 medium-4 columns">
+                <h3 style="padding-left: 30%;">
+                    Skill Levels
+                </h3>
+                <ul class="ranking">
+                    <li>PHP<span class="star-5">* * * * *</span>
+                    </li>
+                    <li>JavaScript<span class="star-5">* * * * *</span>
+                    </li>
+                    <li>CSS3<span class="star-4">* * * *</span>
+                    </li>
+                    <li>HTML5<span class="star-4">* * * *</span>
+                    </li>
+                    <li>XHTML<span class="star-3">* * *</span>
+                    </li>
+                    <li>Servers<span class="star-4">* * * *</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="large-8 medium-6 columns">
+                <h3>
+                    Programming Languages
+                </h3>
+                <p class="ptext">
+                    With PHP, I prefer to write class based module code, as I find it more reusable and cleaner. JavaScript I can write without a framework if it's something that a framework would be overkill on size, I also am very familiar with jQuery and MooTools, also Ender and some of the newer micro libraries. I have released many jQuery and MooTools snippets and plugins that were released open source. Linux servers, I love working on servers and have built and maintained many of them for my personal use or business use. I also love building computers and hacking hardware, I currently have 2 servers in my house just for home media centers. General IT support, I can install software, remove software, viruses, malware, reinstall OSes, hook up your wifi printer, build a NAS device for your home network, whatever the job requires. I do quite a bit of free lance general IT support currently.
+                </p>
+            </div>
+        </div><!-- end row -->
+        <hr>
+        <div class="row"></div><!-- end row -->
 
 <?php include './inc/footer.php'; ?>
