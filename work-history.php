00b41e9a9c2ffc9513a9c127e1762695885054a5
Shawn Crigger <support@s-vizion.com>
2014-04-21 15:55:54 -0400

    styling work history, added hgroups for years and position

--- work-history.php
+++ work-history.php
@@ -14,11 +14,17 @@
 		<section class="row">
 
 			<div class="large-4 medium-4 columns work-history">
-				<h3><a href="http://eaglewebdesigns.com/">Eagle Web Designs, Inc</a></h3>
-				<strong>2009 - 2012 - Full Stack Web Developer</strong>
+					<h3>
+						<a href="http://eaglewebdesigns.com/">Eagle Web Designs, Inc</a>
+					</h3>
+					<hgroup>
+						<h5>Full Stack Web Developer</h5>
+						<h6>2009 - 2012</h6>
+					</hgroup>
+
 				<br>
-				<p class="ptext">
-					I started at Eagle Web Designs as a free-lance programmer doing odd jobs at first then moved to in house senior programmer.
+				<p>
+						I started at Eagle Web Designs as a free-lance programmer doing odd jobs at first then moved to in house senior programmer.
 					Over the years I worked there, I created many very large website's for Real Estate Agency's, Nightclubs, Property Management Company,
 					and anything else that came up. I also fixed issues with servers, troubleshot bugs with existing code, everything but design.
 				</p>
@@ -80,15 +86,19 @@
 			<hr>
 		</section> <!-- end row -->
 		<section class="row">
-			<div class="large-6 medium-5 columns work-history">
+			<div class="large-7 medium-5 columns work-history">
 				<h3>
 					Peachtree Electric
-				</h3><strong>1997 - 2003 - Electrician Helper, Service Electrician</strong><br>
-				<p class="ptext">
+				</h3>
+				<hgroup>
+					<h5>Electrician Helper, Service Electrician</h5>
+					<h6>1997 - 2003</h6>
+				</hgroup>
+				<p>
 					I started as a green helper, doing commerical electric mostly service work but also lots of new commerical construction. My craftsmenship is still seen daily in many stores, malls, outlets and resturants in the Grand Strand Area.
 				</p>
 			</div>
-			<div class="large-4 medium-4 columns work-history">
+			<div class="large-5 medium-4 columns work-history">
 				<h3>
 					Achievements
 				</h3>
@@ -106,15 +116,21 @@
 			<hr>
 		</section>
 		<section class="row">
-			<div class="large-6 medium-5 columns work-history">
+			<div class="large-7 medium-5 columns work-history">
 				<h3>
 					Sandpiper HVAC
-				</h3><strong>2003 - 2007 - HVAC Service Tech and Installer</strong><br>
-				<p class="ptext">
+
+				</h3>
+
+				<hgroup>
+					<h5>HVAC Service Tech and Project Manager</h5>
+					<h6>2003 - 2007</h6>
+				</hgroup>
+				<p>
 					I started as a project manager for Ocean Keys Condo in NMB, SC. In less then a years time I was also running Service Calls, Change Outs, and New Construction in multi-family and single-family dwelling projects.
 				</p>
 			</div>
-			<div class="large-4 medium-4 columns work-history">
+			<div class="large-5 medium-4 columns work-history">
 				<h3>
 					Achievements
 				</h3>
@@ -134,15 +150,18 @@
 			<hr>
 		</section>
 		<section class="row">
-			<div class="large-6 medium-5 columns work-history">
+			<div class="large-7 medium-5 columns work-history">
 				<h3>
-					Free Lance Web Developer
-				</h3><strong>2006 - 2009</strong><br>
-				<p class="ptext">
+					Freelance Web Craftsmen
+				</h3>
+				<hgroup>
+					<h6>2006 - 2009</h6>
+				</hgroup>
+				<p>
 					While working as a HVAC Service tech, I created a very large online community based on online game cheating which used a Pascal based language in a custom compiler. After modifying and running the forums for a few years, and having lost total interest in the game I was developing the cheat for, I started ShawnC.org which was a very small web development company that I ran at night after I got off work. It mostly worked on vBulletin systems as that is what I was building modifications and things for and releasing on vBulletin.ORG
 				</p>
 			</div>
-			<div class="large-4 medium-4 columns work-history">
+			<div class="large-5 medium-4 columns work-history">
 				<h3>
 					Achievements
 				</h3>
@@ -160,11 +179,14 @@
 			<hr>
 		</section>
 		<section class="row">
-			<div class="large-6 medium-5 columns work-history">
+			<div class="large-7 medium-5 columns work-history">
 				<h3>
-					Freelance Developer
-				</h3><strong>2012-2014</strong><br>
-				<p class="ptext">
+					Fullstack Web Craftsmen
+				</h3>
+				<hgroup>
+					<h6>2012-2014</h6>
+				</hgroup>
+				<p>
 					Currently I am working as a Freelance developer, doing everything from Computer Repair, Server Setups, Small Web Projects, Tech Support, and any programming project involving PHP or JavaScript. Mostly working with Wordpress and CodeIgniter based projects lately.
 				</p>
 			</div>
