eeb0ad01ef6f98f9532e12cee81789b734758a45
Shawn Crigger <support@s-vizion.com>
2014-04-20 23:25:56 -0400

    opengraph tags for no reason

--- inc/head.php
+++ inc/head.php
@@ -7,5 +7,18 @@
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" href="css/main.css" />
     <script src="js/vendor/modernizr.js"></script>
+
+
+	<meta http-equiv="imagetoolbar" content="no">
+	<meta http-equiv="MSThemeCompatible" content="No">
+
+	<!-- <meta property="fb:app_id" content="0000000000000"> -->
+	<!-- <meta property="fb:admins" content="1111111"> -->
+	<meta property="og:title" content="Dready Crigger's Resume">
+	<meta property="og:image" content="http://dready-criggs-resume.herokuapp.com/img/heroshot-3.jpg">
+	<meta property="og:site_name" content="Dready Crigger's Resume">
+	<meta property="og:url" content="http://dready-criggs-resume.herokuapp.com/">
+	<meta property="og:description" content="Dready Crigger is a computer craftmens, and this my friends is his awesome resume.">
+	<meta property="og:type" content="website">
   </head>
   <body>
