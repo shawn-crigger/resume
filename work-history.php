6d099671c6d451953b86f78f9846d02edf3766ed
Shawn Crigger <support@s-vizion.com>
2014-04-20 23:07:32 -0400

    adding uls for achivements next to all the jobs

--- work-history.php
+++ work-history.php
@@ -1,146 +1,163 @@
 <?php
   include './inc/head.php';
   include './inc/topbar.php';
-
 ?>
-
-	<div class="panel callout radius">
-	  <h2>Work History</h2>
-	</div>
-
-
-    <div class="row">
-      <div class="large-4 medium-4 columns work-history">
-      	<h3>Eagle Web Designs, Inc</h3>
-		<strong>2009 - 2012 - Full Stack Web Developer</strong><br/>
-		<p class="ptext">
-		  I started at EWD as a free-lance programmer doing odd jobs at first then moved to in house senior programmer. Over the years I worked there, I created many very large website's for Real Estate Agency's, Nightclubs, Property Management Company, and anything else that came up.  I also fixed issues with servers, troubleshot bugs with existing code, everything but design.
-		</p>
-	  </div>
-
-	  <div class="large-4 medium-4 columns work-history">
-	  	<h3>Achievements</h3>
-				  	<ul class="job-achivements">
-					 <li>Created CMS as back-bone for almost all web sites</li>
-					 <li>Build entire website's from sliced psd's</li>
-					 <li>Coded many re-usable modules for common design items, mostly using PHP with JavaScript and CSS3</li>
-					 <li>Migrated websites from previous server to new servers</li>
-					 <li>Repaired any server issues, security issues, and kept all software up to date</li>
-					</ul>
-	  </div>
-	  <div class="large-4 medium-4 columns work-history">
-					<h3>Websites I built at EWD</h3>
-
-					<ul class="portfolio-links">
-					 <li><a href="http://www.celebrationsnitelife.com/">Celebrations Nitelife</a></li>
-
-					 <li><a href="http://www.coastalluxuryproperties.com/">Coastal Luxury Properties</a></li>
-					 <li><a href="http://www.bbatb.com/">Baseball at the Beach</a></li>
-					 <li><a href="http://www.fullreoservices.com/">Full REO Services</a></li>
-
-					 <li><a href="http://www.isleofcandles.com">Isle of Candles</a></li>
-
-					 <li><a href="http://www.ngatour.com/">NGA Tour</a></li>
-					 <li><a href="http://www.myrtlebeachsafari.com">Myrtle Beach Safari</a></li>
-
-					 <li><a href="http://www.tigercensus.com/">Tiger Census</a></li>
-					 <li><a href="http://www.thescentshoppe.com/">The Scent Shoppe</a></li>
-					 </ul>
-			</div>
-			<hr/>
-  		</div>
-
-    <div class="row">
-      <div class="large-4 medium-4 columns work-history">
-  				<h3>Peachtree Electric</h3>
-  				<strong>1997 - 2003 - Electrician Helper, Service Electrician</strong><br />
-  				<p class="ptext">
-  				I started as a green helper, doing commerical electric mostly service work but also lots of new commerical construction.  My craftsmenship is still seen daily in
-  				many stores, malls, outlets and resturants in the Grand Strand Area.
-  				</p>
-  		</div>
-
-  		<div class="large-4 medium-4 columns work-history">
-			<h3>Achievements</h3>
-			<ul class="job-achivements">
-				<li>Took Journeymen Electrician License test, scoring a 98%</li>
-				<li>Project manager in early 2003 for a multi-family dwelling project</li>
-				<li>Troubleshooting, and Repair any electrical problem.</li>
-				<li>Calculating power loads on new dwellings, pulling wire, installing fixtures, all of it.</li>
-			<li></li>
-			<li></li>
-
-			</ul>
-	  	</div>
-		<hr/>
-  	</div>
-
-	<div class="row">
-    	<div class="large-4 medium-4 columns work-history">
-			<h3>Sandpiper HVAC</h3>
-			<strong>2003 - 2007 - HVAC Service Tech and Installer</strong><br/>
-			<p class="ptext">
-			I started as a project manager for Ocean Keys Condo in NMB, SC.  In less then a years time I was also running Service Calls, Change Outs, and New Construction in
-			multi-family and single-family dwelling projects.
-			</p>
+		<div class="panel callout radius">
+			<h2>
+				Work History
+			</h2>
 		</div>
-
-	<div class="large-4 medium-4 columns work-history">
-		<h3>Achievements</h3>
-		<ul class="job-achivements">
-			<li>Plan what needed to be done each day for up to 5 different teams of workers</li>
-			<li>Fabricate Metal, Braze Copper Connections, Start up Units</li>
-			<li>Order stock needed for all the different new construction projects</li>
-			<li>Take Service Calls at all hours, troubleshoot and repair units</li>
-			<li>Sold new units and installed them if the broken unit was beyond repair</li>
-			<li></li>
-			<li></li>
-			<li></li>
-
-		</ul>
-	  </div>
-
-		<hr/>
-	</div>
-
-
-
-	<div class="row">
+		<div class="row">
 			<div class="large-4 medium-4 columns work-history">
-				<h3>Free Lance Web Developer</h3>
-				<strong>2006 - 2009</strong><br />
+				<h3>
+					Eagle Web Designs, Inc
+				</h3><strong>2009 - 2012 - Full Stack Web Developer</strong><br>
 				<p class="ptext">
-				While working as a HVAC Service tech,  I created a very large online community based on online game cheating which used a Pascal based language in a custom compiler.
-				After modifying and running the forums for a few years,  and having lost total interest in the game I was developing the cheat for, I started ShawnC.org which
-				was a very small web development company that I ran at night after I got off work.  It mostly worked on vBulletin systems as that is what I was building modifications and things for and releasing on vBulletin.ORG
+					I started at EWD as a free-lance programmer doing odd jobs at first then moved to in house senior programmer. Over the years I worked there, I created many very large website's for Real Estate Agency's, Nightclubs, Property Management Company, and anything else that came up. I also fixed issues with servers, troubleshot bugs with existing code, everything but design.
 				</p>
 			</div>
-			<hr/>
-
-
-	<div class="large-4 medium-4 columns work-history">
-		<h3>Achievements</h3>
-		<ul class="job-achivements">
-			<li>Created a huge community of online gamers, that I made money off of.</li>
-			<li>Learned alot about web development, which meshed well with my years of programming XP</li>
-			<li>Released many mods and themes for vBulletin Forum Software</li>
-			<li>Lots of vBulletin based jobs were completed during these years.</li>
-		</ul>
-	  </div>
-
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Achievements
+				</h3>
+				<ul class="job-achivements">
+					<li>Created CMS as back-bone for almost all web sites
+					</li>
+					<li>Build entire website's from sliced psd's
+					</li>
+					<li>Coded many re-usable modules for common design items, mostly using PHP with JavaScript and CSS3
+					</li>
+					<li>Migrated websites from previous server to new servers
+					</li>
+					<li>Repaired any server issues, security issues, and kept all software up to date
+					</li>
+				</ul>
+			</div>
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Websites I built at EWD
+				</h3>
+				<ul class="portfolio-links">
+					<li>
+						<a href="http://www.celebrationsnitelife.com/">Celebrations Nitelife</a>
+					</li>
+					<li>
+						<a href="http://www.coastalluxuryproperties.com/">Coastal Luxury Properties</a>
+					</li>
+					<li>
+						<a href="http://www.bbatb.com/">Baseball at the Beach</a>
+					</li>
+					<li>
+						<a href="http://www.fullreoservices.com/">Full REO Services</a>
+					</li>
+					<li>
+						<a href="http://www.isleofcandles.com">Isle of Candles</a>
+					</li>
+					<li>
+						<a href="http://www.ngatour.com/">NGA Tour</a>
+					</li>
+					<li>
+						<a href="http://www.myrtlebeachsafari.com">Myrtle Beach Safari</a>
+					</li>
+					<li>
+						<a href="http://www.tigercensus.com/">Tiger Census</a>
+					</li>
+					<li>
+						<a href="http://www.thescentshoppe.com/">The Scent Shoppe</a>
+					</li>
+				</ul>
+			</div>
+			<hr>
 		</div>
-
-
-        <div class="row">
-  			<div class="large-4 medium-4 columns work-history">
-  				<h3>Freelance Developer</h3>
-  				<strong>2012-2014</strong><br />
-  				<p class="ptext">
-  				Currently I am working as a Freelance developer, doing everything from Computer Repair, Server Setups, Small Web Projects, Tech Support, and any programming project involving PHP or JavaScript.  Mostly working with Wordpress and CodeIgniter based projects lately.
-  				</p>
-  			</div>
-  			<hr/>
-  		</div>
-
-
+		<div class="row">
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Peachtree Electric
+				</h3><strong>1997 - 2003 - Electrician Helper, Service Electrician</strong><br>
+				<p class="ptext">
+					I started as a green helper, doing commerical electric mostly service work but also lots of new commerical construction. My craftsmenship is still seen daily in many stores, malls, outlets and resturants in the Grand Strand Area.
+				</p>
+			</div>
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Achievements
+				</h3>
+				<ul class="job-achivements">
+					<li>Took Journeymen Electrician License test, scoring a 98%
+					</li>
+					<li>Project manager in early 2003 for a multi-family dwelling project
+					</li>
+					<li>Troubleshooting, and Repair any electrical problem.
+					</li>
+					<li>Calculating power loads on new dwellings, pulling wire, installing fixtures, all of it.
+					</li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+		<div class="row">
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Sandpiper HVAC
+				</h3><strong>2003 - 2007 - HVAC Service Tech and Installer</strong><br>
+				<p class="ptext">
+					I started as a project manager for Ocean Keys Condo in NMB, SC. In less then a years time I was also running Service Calls, Change Outs, and New Construction in multi-family and single-family dwelling projects.
+				</p>
+			</div>
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Achievements
+				</h3>
+				<ul class="job-achivements">
+					<li>Plan what needed to be done each day for up to 5 different teams of workers
+					</li>
+					<li>Fabricate Metal, Braze Copper Connections, Start up Units
+					</li>
+					<li>Order stock needed for all the different new construction projects
+					</li>
+					<li>Take Service Calls at all hours, troubleshoot and repair units
+					</li>
+					<li>Sold new units and installed them if the broken unit was beyond repair
+					</li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+		<div class="row">
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Free Lance Web Developer
+				</h3><strong>2006 - 2009</strong><br>
+				<p class="ptext">
+					While working as a HVAC Service tech, I created a very large online community based on online game cheating which used a Pascal based language in a custom compiler. After modifying and running the forums for a few years, and having lost total interest in the game I was developing the cheat for, I started ShawnC.org which was a very small web development company that I ran at night after I got off work. It mostly worked on vBulletin systems as that is what I was building modifications and things for and releasing on vBulletin.ORG
+				</p>
+			</div>
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Achievements
+				</h3>
+				<ul class="job-achivements">
+					<li>Created a huge community of online gamers, that I made money off of.
+					</li>
+					<li>Learned alot about web development, which meshed well with my years of programming XP
+					</li>
+					<li>Released many mods and themes for vBulletin Forum Software
+					</li>
+					<li>Lots of vBulletin based jobs were completed during these years.
+					</li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+		<div class="row">
+			<div class="large-4 medium-4 columns work-history">
+				<h3>
+					Freelance Developer
+				</h3><strong>2012-2014</strong><br>
+				<p class="ptext">
+					Currently I am working as a Freelance developer, doing everything from Computer Repair, Server Setups, Small Web Projects, Tech Support, and any programming project involving PHP or JavaScript. Mostly working with Wordpress and CodeIgniter based projects lately.
+				</p>
+			</div>
+			<hr>
+		</div>
 <?php include './inc/footer.php'; ?>
